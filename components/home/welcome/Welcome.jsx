81158ea7b14d5e6203803c318729f399d68b340b
s4ki3f <s4ki3f@gmail.com>
2023-10-13 09:53:05 +0600

    first

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import {
+   View,
+   Text,
+   TextInput,
+   TouchableOpacity,
+   Image,
+   Flatlist
+  } from 'react-native'
+
+import { useRouter } from 'expo-router';
+
+import styles from './welcome.style';
+import { icons, SIZES } from '../../../constants'; 
+
+const Welcome = () => {
+
+  const router = useRouter();
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Gojo</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+      
+      <View>
+        <TextInput
+          style={styles.searchInput}
+        />
+      </View>
+    </View>
+  )
+}
+
+export default Welcome
---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import profile from "../assets/images/gojo.jpg";
+
+export default {
+  profile,
+};
